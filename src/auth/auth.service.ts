d5b7d4b3827e8fb05964d959796745ab111e2946
darmond157 <bardiarezaeikalantari@gmail.com>
2024-08-01 00:23:27 +0330

    feat: add signup and signin processes with database

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,45 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-    signup() {
-        return { msg: "signup" }
+    constructor(private prisma: PrismaService) {
+
+    }
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                }
+            })
+            return user
+        }
+        catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken')
+                }
+            }
+        }
     }
 
-    signin() {
-        return { msg: "signin" }
+    async signin(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        })
+        if (!user) throw new ForbiddenException(('Credentials incorrect'))
+
+        const passwordMatches = await argon.verify(user.hash, dto.password)
+        if (!passwordMatches) throw new ForbiddenException(('Credentials incorrect'))
+
+        return user;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+
+  //we can use this for not passing arguments beside exact ones we defined on class
+  // app.useGlobalPipes(new ValidationPipe({
+  //   whitelist: true
+  // }))
+
   await app.listen(3000);
 }
 bootstrap();
